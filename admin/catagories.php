daf331428cb1dff57c9f0f5e80a3f9db8f6ab13d
Al Mahfuz <almahfuz380@gmail.com>
2018-10-15 22:48:16 +0600

    dynamic catagory done

--- admin/catagories.php
+++ admin/catagories.php
@@ -31,7 +31,27 @@
                     </h1>
 
                     <div class="col-xs-6">
-                        <form action="">
+
+                        <?php
+                            if(isset($_POST['submit'])){
+
+                                $catTitle = $_POST['cat_title'];
+
+                                if($catTitle == "" || empty($catTitle)){
+                                    echo "This field should not be empty";
+                                }else{
+                                    $query = "INSERT INTO catagories(cat_title)"; // inserting data into cat_title column of database
+                                    $query .= "VALUE ('{$catTitle}')";
+
+                                    $create_catagory_query = mysqli_query($connection,$query);
+                                    if(!$create_catagory_query){
+                                        die('QUERY FAILED'. mysqli_error($connection));
+                                    }
+                                }
+                            }
+                        ?>
+
+                        <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat-title">Add Catagory</label>
                                 <input class="form-control" type="text" name="cat_title">
@@ -41,6 +61,46 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Catagory">
                             </div>
                         </form>
+                    </div> <!-- add catagory form -->
+
+                    <div class="col-xs-6">
+
+                        <?php
+
+                        $query = "SELECT * FROM catagories";
+                        $select_catagories = mysqli_query($connection,$query);
+
+
+                        ?>
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Catagory Title</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+
+                            <?php
+                            while ($row = mysqli_fetch_assoc($select_catagories)){
+                                $catId = $row['cat_id'];
+                                $catTitle = $row['cat_title'];
+
+                                echo "<tr>";
+
+
+                                echo "<td>{$catId}</td>";
+                                echo "<td>{$catTitle}</td>";
+                                echo "</tr>";
+                            }
+
+                            ?>
+
+
+                            </tbody>
+                        </table>
                     </div>
 
                 </div>
