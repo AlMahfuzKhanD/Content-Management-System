35a90da57efa1339b7a48244679b0dd4675049a7
Al Mahfuz <almahfuz380@gmail.com>
2018-10-17 22:24:20 +0600

    working on post module

---
+++ admin/includes/view_all_posts.php
@@ -0,0 +1,51 @@
+<table class="table table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>Author</th>
+        <th>Title</th>
+        <th>Catagory</th>
+        <th>Status</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Comments</th>
+        <th>Date</th>
+    </tr>
+    </thead>
+
+    <tbody>
+
+    <?php
+    $query = "SELECT * FROM posts";
+    $select_posts = mysqli_query($connection,$query);
+
+    while ($row = mysqli_fetch_assoc($select_posts)) {
+        $postId = $row['postId'];
+        $postAuthor = $row['postAuthor'];
+        $postTitle = $row['postTitle'];
+        $postCatagoryId = $row['postCatagoryId'];
+        $postStatus = $row['postStatus'];
+        $postImage = $row['postImage'];
+        $postTags= $row['postTags'];
+        $postCommentCount = $row['postCommentCount'];
+        $postDate = $row['postDate'];
+        echo "<tr>";
+        echo "<td>{$postId}</td>";
+        echo "<td>{$postAuthor}</td>";
+        echo "<td>{$postTitle}</td>";
+        echo "<td>{$postCatagoryId}</td>";
+        echo "<td>{$postStatus}</td>";
+        echo "<td><img width='150' src='../images/{$postImage}' alt='image not found'></td>";
+        echo "<td>{$postTags}</td>";
+        echo "<td>{$postCommentCount}</td>";
+        echo "<td>{$postDate}</td>";
+
+
+        echo "</tr>";
+
+    }
+    ?>
+
+
+    </tbody>
+</table>
---
+++ admin/posts.php
@@ -0,0 +1,57 @@
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
+
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+
+
+                    <h1 class="page-header">
+                    Welcome To Admin
+                    <small>Author</small>
+                    </h1>
+
+                    <?php
+                        if(isset($_GET['source'])){
+                            $source = $_GET['source'];
+                        }else{
+                            $source = '';
+                        }
+
+                        switch ($source){
+                            case '34';
+                            echo "nice";
+                            break;
+
+                            case '34';
+                                echo "nice";
+                                break;
+
+                            case '34';
+                                echo "nice";
+                                break;
+                            default:
+
+                                include "includes/view_all_posts.php";
+                                break;
+                        }
+                    ?>
+
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+<?php include "includes/admin_footer.php"; ?>
