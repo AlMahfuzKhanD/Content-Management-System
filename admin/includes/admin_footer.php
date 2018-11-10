c977db14e3b793c3b0027abaacbf7d600631554a
Al Mahfuz <almahfuz380@gmail.com>
2018-11-10 22:51:27 +0600

    dashboard chart added

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -8,6 +8,7 @@
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
 
+
 </body>
 
 </html>
--- admin/index.php
+++ admin/index.php
@@ -28,11 +28,6 @@
                 </div>
                 <!-- /.row -->
 
-
-
-
-                <!-- /.row -->
-
                 <div class="row">
                     <div class="col-lg-3 col-md-6">
                         <div class="panel panel-primary">
@@ -163,7 +158,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="categories.php">
+                            <a href="catagories.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">View Details</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -173,6 +168,44 @@
                         </div>
                     </div>
                 </div>
+
+
+                <!-- /.row -->
+
+                <div class="row">
+
+                    <script type="text/javascript">
+                        google.charts.load('current', {'packages':['bar']});
+                        google.charts.setOnLoadCallback(drawChart);
+
+                        function drawChart() {
+                            var data = google.visualization.arrayToDataTable([
+                                ['Year', 'Sales', 'Expenses', 'Profit'],
+                                ['2014', 1000, 400, 200],
+                                ['2015', 1170, 460, 250],
+                                ['2016', 660, 1120, 300],
+                                ['2017', 1030, 540, 350]
+                            ]);
+
+                            var options = {
+                                chart: {
+                                    title: 'Company Performance',
+                                    subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                                }
+                            };
+
+                            var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                            chart.draw(data, google.charts.Bar.convertOptions(options));
+                        }
+                    </script>
+
+
+                    <div id="columnchart_material" style="width:'auto'; height: 500px;"></div>
+
+                </div>
+
+
                 <!-- /.row -->
 
 
